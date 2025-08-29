848f73d65d5d77792fab0938bb25827ca8a746c9
LinThantAung16 <linthantag16@gmail.com>
2025-08-29 13:48:49 +0100

    new branch

--- RestaurantPOS.Domain/obj/Debug/net8.0/RestaurantPOS.Domain.AssemblyInfo.cs
+++ RestaurantPOS.Domain/obj/Debug/net8.0/RestaurantPOS.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RestaurantPOS.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6dfcdab949efc37f14e8a409d94f8d5c7a241298")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c60d4886eed145d73d7146a010c9e16ea001c81")]
 [assembly: System.Reflection.AssemblyProductAttribute("RestaurantPOS.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RestaurantPOS.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
